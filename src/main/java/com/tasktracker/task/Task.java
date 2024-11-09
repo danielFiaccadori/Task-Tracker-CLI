ff757458214e9d72b05d29a6056a6db4094ca922
danielFiaccadori <daniel.borborema2@hotmail.com>
2024-11-08 22:20:50 -0300

    Task entity creation

---
+++ src/main/java/com/tasktracker/task/Task.java
@@ -0,0 +1,59 @@
+package com.tasktracker.task;
+
+import java.time.Instant;
+import java.util.Objects;
+
+public class Task {
+
+    private Integer id;
+    private String description;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    public Task() {
+
+    }
+
+    public Task(String description) {
+        this.description = description;
+        this.createdAt = Instant.now();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(id, task.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+}
